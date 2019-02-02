06d4c0222e87af9b2c0dcab5b85b1c1241cf3c25
chemica1 <46700473+chemica1@users.noreply.github.com>
2019-02-02 18:57:58 +0900

    fix : movie ranking API

--- src/App.js
+++ src/App.js
@@ -11,21 +11,34 @@ class App extends Component {
 //update : componentWillReceiveProps() -> shouldComponentUpdate() -> componentWillUpdate() -> render() -> componentDidUpdate
  
   state={
-      
+
   }
    componentDidMount(){
-    fetch('https://yts.am/api/v2/list_movies.json?sort_by=download_count')
-    .then(potato => potato.json()) //2진코드를 json형태로 바꿔주고
-    .then(meoho => console.log(meoho)) //then엔 함수1개만 넣어야됨.
-    .catch(err => console.log(err))  //에러보상코드
+     this._getMovies();
   }
 
 
-  renderMovieInfo=()=>{   //react는 긴 배열에게 key값을 주라고 콘솔로 경고한다. 그래서 index 넣음
-    const movieInfo = this.state.movieInfo.map((movieInfo, index)=>{
-      return <Movie title={movieInfo.title} image={movieInfo.poster} key={index}/> //이 index는 리액트에서 기본으로 주는 함수나 변수같은녀석임. react내장기능
+  renderMovieInfo=()=>{   //index를 이용해 key값을 부여하면 좀 느리니까 api에 내장된 key값을 쓰기로한다.
+    const movieInfo = this.state.movieInfo.map(movieInfo=>{
+      return <Movie title={movieInfo.title} image={movieInfo.large_cover_image} key={movieInfo.id}/> 
     })
     return movieInfo
+  }
+
+  _getMovies = async()=>{
+    const movies = await this._callApi()   //this._callApi함수 작업이 끝나길 기다렸다가 movies변수에 집어넣는거임
+    this.setState({
+      movieInfo : movies   //state에 movieInfo가 없어도 알아서 만들어주네
+    })
+
+  }
+
+  _callApi = () =>{
+    return fetch('https://yts.am/api/v2/list_movies.json?sort_by=download_count') //node공부할때 get방식으로 들온거 분석하던 그 코드를 es6에선 이렇게 쉽게 할수있다.
+    .then(potato => potato.json()) //2진코드를 json형태로 바꿔주고
+    .then(meoho => meoho.data.movies) //then엔 함수1개만 넣어야됨.
+    .catch(err => console.log(err))  //에러보상코드  //then..then..해서 여기까지 끝나야지 비로소 return한다.
+
   }
 
    render() {  
